3396280cbd1b036d12fb4b04029d039a138db176
Ali12555 <bderali2005@gmail.com>
2025-06-28 19:47:16 +0300

    ..

--- api/controllers/user.controler.js
+++ api/controllers/user.controler.js
@@ -1,37 +1,30 @@
 const USER_MODEL = require("../models/user.models");
  
- const createUser = (req, res) => {
+
+ const createUser = async (req, res) => {
    const { Email, Password, Name } = req.body;
  
-   USER_MODEL.create({
-     Name: Name,
-     Password: Password,
-     Email: Email,
-   })
-     .then((createRes) => {
-       res.status(200).json(createRes);
-     })
-     .catch((e) => res.status(500).json({ error: true, errorMessage: e }));
+   try {
+     const user = await USER_MODEL.create({
+       Name,
+       Email,
+       Password,
+     });
  
+     res.status(200).json({
+       success: true,
+       massage: "user created",
+       data: user,
+     });
+   } catch (e) {
+     res.status(500).json({
+       success: false,
+       massage: "user not created",
+       data: e,
+     });
+   }
  };
 
-// const Login = (req, res) => {
-//    const { Email, Password} = req.body;
- 
-//    USER_MODEL.findOne({
-//      Email: Email,
-//      Password: Password
-//    })
-//      .then((user) => {
-//       if(user){
-//         res.status(200).json(user)
-//       } else {
-//         res.status(404).json({ error: true, errorMessage: "User not found" });
-//       }
-//      })
-//      .catch((e) => res.status(500).json({ error: true, errorMessage: e }));
-
-//  };
 const Login = async(req, res) => {
   const { Email, Password} = req.body;
  try{
